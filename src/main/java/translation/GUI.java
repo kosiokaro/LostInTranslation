e75dc57bc3451e06baccc316382c1f6db5576f01
Eileen1222 <yihe.xiao@mail.utoronto.ca>
2025-09-23 23:29:41 -0400

    new version GUI, deleted the submit button

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -41,50 +41,75 @@ public class GUI {
             languagePanel.add(languageBox);
 
 
-            JPanel buttonPanel = new JPanel();
-            JButton submit = new JButton("Submit");
-            buttonPanel.add(submit);
+//            JPanel buttonPanel = new JPanel();
+//            JButton submit = new JButton("Submit");
+//            buttonPanel.add(submit);
 
-            JLabel resultLabelText = new JLabel("Translation:");
-            buttonPanel.add(resultLabelText);
-            JLabel resultLabel = new JLabel("\t\t\t\t\t\t\t");
-            buttonPanel.add(resultLabel);
+            JPanel resultPanel = new JPanel();
+            JLabel resultLabel = new JLabel("Translation:");
+            JLabel resultL = new JLabel();
+            resultPanel.add(resultLabel);
+            resultPanel.add(resultL);
+
+
+            // 创建更新翻译的方法
+            Runnable updateTranslation = () -> {
+                String selectedCountryName = (String) countryBox.getSelectedItem();
+                String selectedLanguageName = (String) languageBox.getSelectedItem();
+
+                if (selectedCountryName != null && selectedLanguageName != null) {
+                    String countryCode = cc.fromCountry(selectedCountryName);
+                    String languageCode = lc.fromLanguage(selectedLanguageName);
+
+                    if (countryCode != null && languageCode != null) {
+                        String result = translator.translate(countryCode, languageCode);
+                        if (result != null) {
+                            resultL.setText(result);
+                        }
+                    }
+                }
+            };
 
             //listener for language and country
             countryBox.addActionListener(e -> {
+                // 更新翻译结果
+                updateTranslation.run();
+            });
 
+            languageBox.addActionListener(e -> {
+                updateTranslation.run();
             });
 
 
             // adding listener for when the user clicks the submit button
-            submit.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String language = (String) languageBox.getSelectedItem();
-                    String country = (String) countryBox.getSelectedItem();
-
-                    // for now, just using our simple translator, but
-                    // we'll need to use the real JSON version later.
-                    //Translator translator = new CanadaTranslator();
-                    String languageCode = lc.fromLanguage(language);
-                    String countryCode = cc.fromCountry(country);
-
-
-                    String result = translator.translate(countryCode, languageCode);
-                    if (result == null) {
-                        result = "no translation found!";
-                    }
-                    resultLabel.setText(result);
-
-                }
-
-            });
+//            submit.addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//                    String language = (String) languageBox.getSelectedItem();
+//                    String country = (String) countryBox.getSelectedItem();
+//
+//                    // for now, just using our simple translator, but
+//                    // we'll need to use the real JSON version later.
+//                    //Translator translator = new CanadaTranslator();
+//                    String languageCode = lc.fromLanguage(language);
+//                    String countryCode = cc.fromCountry(country);
+//
+//
+//                    String result = translator.translate(countryCode, languageCode);
+//                    if (result == null) {
+//                        result = "no translation found!";
+//                    }
+//                    resultLabel.setText(result);
+//
+//                }
+//
+//            });
 
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
             mainPanel.add(countryPanel);
             mainPanel.add(languagePanel);
-            mainPanel.add(buttonPanel);
+            mainPanel.add(resultPanel);
 
             JFrame frame = new JFrame("Country Name Translator");
             frame.setContentPane(mainPanel);
